095a668b7ca6c684b9649712a57028d652190104
Justin Swaney <jswaney@mit.edu>
2018-08-09 11:55:12 -0700

    Add display range adjustment and channel radio btns

--- psviewer/main.py
+++ psviewer/main.py
@@ -3,15 +3,16 @@ import argparse
 from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
 import psviewer.terastitcher as ts
 import psviewer.raw as raw
 
 
+# Hold the state of the current FOV
 c_idx = 0
 z_idx = 0
 y_idx = 0
 x_idx = 0
-fig, ax = plt.subplots(figsize=(8, 8))
 
 cs = None
 zs = None
@@ -23,9 +24,70 @@ zstep = None
 pixel_width_um = None
 clim = None
 data_path = None
+img = None
+
+ax_img = None
+fig, ax = plt.subplots(figsize=(8, 8))
+plt.subplots_adjust(left=0.25, bottom=0.25)
+ax_cmin = plt.axes([0.25, 0.1, 0.65, 0.03])
+ax_cmax = plt.axes([0.25, 0.15, 0.65, 0.03])
+ax_auto = plt.axes([0.8, 0.025, 0.1, 0.04])
+ax_radio = plt.axes([0.025, 0.5, 0.15, 0.15])
+slider_cmin = Slider(ax_cmin, 'Min Intensity', 0, 2**12-1, valinit=0, valstep=1, valfmt='%d')
+slider_cmax = Slider(ax_cmax, 'Max Intensity', 0, 2**12-1, valinit=1000, valstep=1, valfmt='%d')
+auto_btn = Button(ax_auto, 'Auto')
+radio_btn = None
+
+
+def adjust_cmin(event):
+    global clim
+    cmin = slider_cmin.val
+    cmax = clim[1]
+    clim = [cmin, cmax]
+    if cmin < cmax:
+        ax_img.set_clim(vmin=cmin, vmax=cmax)
+    else:
+        slider_cmin.set_val(cmax-1)
+        ax_img.set_clim(vmin=cmax-1, vmax=cmax)
+
+
+def adjust_cmax(event):
+    global clim
+    cmin = clim[0]
+    cmax = slider_cmax.val
+    clim = [cmin, cmax]
+    if cmin < cmax:
+        ax_img.set_clim(vmin=cmin, vmax=cmax)
+    else:
+        slider_cmax.set_val(cmin+1)
+        ax_img.set_clim(vmin=cmin, vmax=cmin+1)
+
+
+def auto_adjust_clim(event):
+    global clim
+    cmin = img.min()
+    cmax = img.max()/2
+    if cmin < cmax:
+        clim = [cmin, cmax]
+        slider_cmin.set_val(cmin)
+        slider_cmax.set_val(cmax)
+
+
+def change_channel(event):
+    global c_idx
+    new_c_idx = int(event[-1])
+    if new_c_idx != c_idx:
+        c_idx = new_c_idx
+        plot_img()
+
+
+slider_cmin.on_changed(adjust_cmin)
+slider_cmax.on_changed(adjust_cmax)
+auto_btn.on_clicked(auto_adjust_clim)
 
 
 def plot_img():
+    global ax_img, img
     c, z, y, x = cs[c_idx], zs[z_idx], ys[y_idx], xs[x_idx]
     img_path = ts.build_path(data_path, c, z, y, x, fmt)
     if fmt in ['tif', 'tiff']:
@@ -34,14 +96,14 @@ def plot_img():
         img = raw.raw_imread(img_path)
     width = pixel_width_um * img.shape[0]
     x_um, y_um, z_um = x/10, y/10, z/10
-    ax.imshow(img, extent=[x_um, x_um+width, y_um+width, y_um], clim=clim, cmap='gray', interpolation='none')
+    ax_img = ax.imshow(img, extent=[x_um, x_um+width, y_um+width, y_um], clim=clim, cmap='gray', interpolation='none')
     ax.set_title(f'{c}: z-slice ({z_idx+1}/{len(zs)}) {z_um} um')
     ax.set_xlabel(f'x-tile {x_idx+1} / {len(xs)}')
     ax.set_ylabel(f'y-tile {y_idx+1} / {len(ys)}')
     fig.canvas.draw()
 
 
-def press(event):
+def keypress(event):
     global c_idx, z_idx, y_idx, x_idx
     if event.key == ',':
         if z_idx >= zstep:
@@ -73,12 +135,16 @@ def press(event):
         plot_img()
     elif event.key in [str(i) for i in range(len(cs))]:
         c_idx = int(event.key)
+        radio_btn.set_active(c_idx)
         plot_img()
-    # print(f'Color_{c_idx}: z = {z_idx}, y = {y_idx}, x = {x_idx}')
+    elif event.guiEvent.char in [str(i) for i in range(len(cs))]:  # catch numpad too
+        c_idx = int(event.guiEvent.char)
+        radio_btn.set_active(c_idx)
+        plot_img()
 
 
 def main():
-    global cs, zs, ys, xs, fmt, pixel_width_um, clim, data_path, zstep
+    global cs, zs, ys, xs, fmt, pixel_width_um, clim, data_path, zstep, radio_btn
     parser = argparse.ArgumentParser(description='Pre-stitcher viewer')
     parser.add_argument('--data-path', type=str, help='input path to unstitcher terastitcher volume', default='.')
     parser.add_argument('--zstep', type=int, help='number of frames to move in z', default=100)
@@ -95,7 +161,9 @@ def main():
     fmt = ts.get_format(data_path)
     cs, zs, ys, xs = ts.get_czyx(data_path)
 
-    fig.canvas.mpl_connect('key_press_event', press)
+    radio_btn = RadioButtons(ax_radio, cs, active=0)
+    radio_btn.on_clicked(change_channel)
+    fig.canvas.mpl_connect('key_press_event', keypress)
     plot_img()
     plt.show()
 
